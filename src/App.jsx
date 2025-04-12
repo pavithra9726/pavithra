f1ae3fc4321d9e52fc4d512ed8f20867fd050fad
pavithra9726 <royalkingmathan123@gmail.com>
2025-04-12 10:54:35 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Profilecard from './Profilecard';
+// import Profilecard from './Profilecard';
 import GridView from './GridView';
 import Todo from './Todo';
 import Navbar from './Navbar';
@@ -10,6 +10,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import store from './store';
 import {Provider} from "react-redux";
 import Home from './home';
+import ProfileCard from './ProfileCard';
 
 const profilelist = {
   name: "pavithra",
@@ -26,7 +27,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/todo" element={<Todo />} /> */}
-        <Route path="/profilecard" element={<Profilecard profile={profilelist}/>
+        <Route path="/profilecard" element={<ProfileCard profile={profilelist}/>
 } />
         <Route path="/gridview" element={<GridView />} />
 
